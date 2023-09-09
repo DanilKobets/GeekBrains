9f454e545df9ac03696cbae1316e3c9d6536d872
Danil Kobets <kobec.danil@gmail.com>
2023-09-09 21:52:00 +0300

    python

--- Python/PythonHomeWork/PythonHomeWork_7.py
+++ Python/PythonHomeWork/PythonHomeWork_7.py
@@ -10,7 +10,7 @@
 # **Ввод:** пара-ра-рам рам-пам-папам па-ра-па-да    
 #     **Вывод:** Парам пам-пам  
 
-
+#Вариант 1
 rhymes = 'пара-ра-рам рам-пам-папам па-ра-па-дам'
 # аеёиоуыэюя
 def vinni(vinni_pyx):
@@ -18,6 +18,27 @@ def vinni(vinni_pyx):
     return 'Парам пам-пам' if len(set(map(lambda count: len(count), phrase_vinni))) == 1 else 'Пам парам'
 
 print(vinni(rhymes))
+
+
+#Вариант 2 
+def func(str):
+    str = str.lower().split(' ')
+    my_list = list()
+    for word in str:
+        result = 0
+        for i in word:
+            if i in vowels:
+                result += 1
+        my_list.append(result)
+    return len(my_list) == my_list.count(my_list[0])
+
+
+vowels = ['a', 'e', 'i', 'o', 'u', 'y', 'а', 'е', 'ё', 'и', 'о', 'у', 'ы', 'э', 'ю', 'я']
+str = input('Введите кричалку Винни-Пуха: ')
+if func(str):
+    print('Парам пам-пам')
+else:
+    print('Пам парам')
 
 
 # Задача 36: Напишите функцию print_operation_table(operation, num_rows=6, num_columns=6), 
@@ -38,9 +59,33 @@ print(vinni(rhymes))
 # 5 10 15 20 25 30
 # 6 12 18 24 30 36
 
+
+#Вариант 1
+
 def print_operation_table(operation, num_rows=6, num_columns=6):
     row_list, colum_list = [x for x in range(1, num_rows + 1)], [y for y in range(1, num_columns + 1)] #создание сетки
     finish_list = [[operation(row, col) for row in row_list] for col in colum_list] #
     for row in finish_list:
         print(row)
 print_operation_table(lambda x, y: x * y) 
+
+
+#Вариант 2
+def print_operation_table(operation, num_rows=6, num_columns=6):
+    a = [[operation(i, j) for j in range(1, num_columns + 1)] for i in range(1, num_rows + 1)]
+    for i in a:
+        print(*[f'{x: > 3}' for x in i])
+
+print_operation_table(lambda x, y: x * y)
+
+
+# Решение без магических чисел. Так-то оно получше будет...
+def print_operation_table(operation):
+    a = [[operation(i, j) for j in range(1, columns + 1)] for i in range(1, rows + 1)]
+    for i in a:
+        print(*[f'{x:>3}' for x in i])
+
+
+rows = int(input('Введите количество строк: '))
+columns = int(input('Введите количество столбцов: '))
+print_operation_table(lambda x, y: x * y)
